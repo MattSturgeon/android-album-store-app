210e4db8a7fd01bf9b20f71afbb349d19d311a90
Matt Sturgeon <matt@sturgeon.me.uk>
2024-12-16 12:17:33 +0000

    feat(service): add `listAlbums`
    Gets all Albums from the API

--- app/src/main/java/com/northcoders/albumstore/service/AlbumApiService.java
+++ app/src/main/java/com/northcoders/albumstore/service/AlbumApiService.java
@@ -1,4 +1,14 @@
 package com.northcoders.albumstore.service;
 
+import com.northcoders.albumstore.model.AlbumDTO;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface AlbumApiService {
+
+    @GET("albums")
+    Call<List<AlbumDTO>> listAlbums();
 }
