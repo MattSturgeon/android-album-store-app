20d2a91ed7e1ed301e947985098da6124e5cfab6
Matt Sturgeon <matt@sturgeon.me.uk>
2024-12-18 18:38:18 +0000

    feat(view): set up bindings & model in `AddNewAlbumActivity`

--- app/src/main/java/com/northcoders/albumstore/ui/addalbum/AddNewAlbumActivity.java
+++ app/src/main/java/com/northcoders/albumstore/ui/addalbum/AddNewAlbumActivity.java
@@ -7,10 +7,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.northcoders.albumstore.R;
+import com.northcoders.albumstore.databinding.ActivityAddNewAlbumBinding;
+import com.northcoders.albumstore.model.AlbumRequestDTO;
+import com.northcoders.albumstore.ui.mainactivity.MainActivityViewModel;
 
 public class AddNewAlbumActivity extends AppCompatActivity {
+
+    private ActivityAddNewAlbumBinding binding;
+    private MainActivityViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +30,11 @@ public class AddNewAlbumActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        AlbumRequestDTO album = new AlbumRequestDTO();
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_add_new_album);
+        viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+        binding.setClickHandlers(new AddAlbumClickHandlers(viewModel, this, album));
+        binding.setAlbum(album);
     }
 }
