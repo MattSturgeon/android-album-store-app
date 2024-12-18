1aca87ab764f968d241a0a30332ca2d886c9b5f1
Matt Sturgeon <matt@sturgeon.me.uk>
2024-12-18 18:06:49 +0000

    feat(view): initial `add_new_album` click handlers

---
+++ app/src/main/java/com/northcoders/albumstore/ui/addalbum/AddAlbumClickHandlers.java
@@ -0,0 +1,44 @@
+package com.northcoders.albumstore.ui.addalbum;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.northcoders.albumstore.model.AlbumRequestDTO;
+import com.northcoders.albumstore.ui.mainactivity.MainActivity;
+import com.northcoders.albumstore.ui.mainactivity.MainActivityViewModel;
+
+public class AddAlbumClickHandlers {
+
+    private static final String TAG = "ClickHandlers";
+    private AlbumRequestDTO album;
+    private Context context;
+    private MainActivityViewModel mainActivityViewModel;
+
+    public AddAlbumClickHandlers(MainActivityViewModel mainActivityViewModel, Context context, AlbumRequestDTO album) {
+        this.mainActivityViewModel = mainActivityViewModel;
+        this.context = context;
+        this.album = album;
+    }
+
+    public void onSubmit(View view) {
+        // TODO: validate album has required fields
+        // Show a toast and/or inline validation error
+
+        Log.i(TAG, "tapped submit");
+        Toast.makeText(context, "Submitting...", Toast.LENGTH_SHORT).show();
+
+        AlbumRequestDTO copy = album.clone();
+        mainActivityViewModel.addAlbum(copy);
+    }
+
+    public void onCancel(View view) {
+        Log.i(TAG, "tapped cancel");
+        Toast.makeText(context, "Canceling...", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(context, MainActivity.class);
+        context.startActivity(intent);
+    }
+
+}
