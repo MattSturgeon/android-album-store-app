c7cc1793dc5a436be81834662804310f5675a573
Matt Sturgeon <matt@sturgeon.me.uk>
2024-12-18 11:02:25 +0000

    feat(view): add a fallback album art

--- app/src/main/java/com/northcoders/albumstore/ui/bindings/AlbumBindingAdapters.java
+++ app/src/main/java/com/northcoders/albumstore/ui/bindings/AlbumBindingAdapters.java
@@ -1,5 +1,7 @@
 package com.northcoders.albumstore.ui.bindings;
 
+import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,16 +14,24 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class AlbumBindingAdapters {
+    private static final String TAG = AlbumBindingAdapters.class.getSimpleName();
+
     @BindingAdapter("android:text")
     public static void setArtistText(TextView view, List<Artist> artists) {
         view.setText(artistsToString(artists));
     }
 
-    @BindingAdapter("loadImage")
-    public static void getImageUrl(ImageView view, String url) {
-        Glide.with(view.getContext())
-                .load(url)
-                .into(view);
+    @BindingAdapter(value = {"loadImage", "fallbackImage"}, requireAll = false)
+    public static void getImageUrl(ImageView view, String url, Drawable fallbackImage) {
+        if (url != null) {
+            Glide.with(view.getContext())
+                    .load(url)
+                    .into(view);
+        } else if (fallbackImage != null) {
+            view.setImageDrawable(fallbackImage);
+        } else {
+            Log.w(TAG, "attempted to load image with no url or fallback");
+        }
     }
 
     private static String artistsToString(List<Artist> artists) {
