905b9df0950b6758893f82c1ff5fd58cdcb721c7
Matt Sturgeon <matt@sturgeon.me.uk>
2024-12-19 15:04:17 +0000

    feat(service): add PUT & DELETE methods

--- app/src/main/java/com/northcoders/albumstore/model/AlbumRepository.java
+++ app/src/main/java/com/northcoders/albumstore/model/AlbumRepository.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.lifecycle.MutableLiveData;
 
 import com.northcoders.albumstore.service.RetrofitInstance;
@@ -71,6 +72,75 @@ public class AlbumRepository {
         };
         RetrofitInstance.getService()
                 .addAlbum(album)
+                .enqueue(callback);
+    }
+
+    public void updateAlbum(Album album) {
+        AlbumRequestDTO dto = AlbumRequestDTO.from(album);
+        updateAlbum(album.getId(), dto);
+    }
+
+    public void updateAlbum(long id, AlbumRequestDTO album) {
+        Callback<Album> callback = new Callback<>() {
+            @Override
+            public void onResponse(@NonNull Call<Album> call, @NonNull Response<Album> response) {
+                Album album = response.body();
+
+                if (album == null) {
+                    Log.e(TAG, String.format("PUT /albums/%d request was successful, but response contains no album", id));
+                    Toast.makeText(application, "Something went wrong!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (album.getId() != id) {
+                    Log.e(TAG, String.format("PUT /albums/%d request was successful, but response has different id (%d)", id, album.getId()));
+                }
+
+                String text = "Successfully updated " + album.getTitle();
+                Log.i(TAG, text + " with id " + id);
+                Toast.makeText(application, text, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<Album> call, @NonNull Throwable throwable) {
+                Log.e(TAG, Optional.ofNullable(throwable.getMessage()).orElse("Unknown error"));
+                Toast.makeText(application, "Something went wrong!", Toast.LENGTH_SHORT).show();
+            }
+        };
+        RetrofitInstance.getService()
+                .updateAlbum(id, album)
+                .enqueue(callback);
+    }
+
+    public void removeAlbum(long id) {
+        removeAlbum(id, (String) null);
+    }
+
+    public void removeAlbum(long id, AlbumRequestDTO album) {
+        removeAlbum(id, album.getTitle());
+    }
+
+    public void removeAlbum(Album album) {
+        removeAlbum(album.getId(), album.getTitle());
+    }
+
+    public void removeAlbum(long id, @Nullable String title) {
+        Callback<Void> callback = new Callback<>() {
+            @Override
+            public void onResponse(@NonNull Call<Void> call, @NonNull Response<Void> response) {
+                String text = "Successfully removed album" + (title == null ? "" : " " + title);
+                Log.i(TAG, text + " with id " + id);
+                Toast.makeText(application, text, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<Void> call, @NonNull Throwable throwable) {
+                Log.e(TAG, Optional.ofNullable(throwable.getMessage()).orElse("Unknown error"));
+                Toast.makeText(application, "Something went wrong!", Toast.LENGTH_SHORT).show();
+            }
+        };
+        RetrofitInstance.getService()
+                .removeAlbum(id)
                 .enqueue(callback);
     }
 }
--- app/src/main/java/com/northcoders/albumstore/service/AlbumApiService.java
+++ app/src/main/java/com/northcoders/albumstore/service/AlbumApiService.java
@@ -7,8 +7,11 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface AlbumApiService {
 
@@ -17,4 +20,10 @@ public interface AlbumApiService {
 
     @POST("albums")
     Call<Album> addAlbum(@Body AlbumRequestDTO album);
+
+    @PUT("albums/{id}")
+    Call<Album> updateAlbum(@Path("id") long id, @Body AlbumRequestDTO album);
+
+    @DELETE("albums/{id}")
+    Call<Void> removeAlbum(@Path("id") long id);
 }
